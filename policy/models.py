28bce6720b8dbf45a82c517088a997831b129a3d
Eric Darchis <darchis@pobox.com>
2020-04-07 19:19:42 +0200

    Add get_queryset for FHIR fine grained security

--- policy/models.py
+++ policy/models.py
@@ -1,15 +1,19 @@
 import uuid
+
+from django.conf import settings
 from django.db import models
 from core import fields
 from core.models import Officer
+from graphql import ResolveInfo
 from insuree.models import Family
+from location.models import UserDistrict
 from product.models import Product
+from core import models as core_models
 
 
-class Policy(models.Model):
+class Policy(core_models.VersionedModel):
     id = models.AutoField(db_column='PolicyID', primary_key=True)
     uuid = models.CharField(db_column='PolicyUUID', max_length=36, default=uuid.uuid4, unique = True)
-    legacy_id = models.IntegerField(db_column='LegacyID', blank=True, null=True)
 
     stage = models.CharField(db_column='PolicyStage', max_length=1, blank=True, null=True)
     status = models.SmallIntegerField(db_column='PolicyStatus', blank=True, null=True)
@@ -23,9 +27,6 @@ class Policy(models.Model):
 
     product = models.ForeignKey(Product, models.DO_NOTHING, db_column='ProdID', related_name="policies")
     officer = models.ForeignKey(Officer, models.DO_NOTHING, db_column='OfficerID', blank=True, null=True, related_name="policies")
-
-    validity_from = fields.DateTimeField(db_column='ValidityFrom')
-    validity_to = fields.DateTimeField(db_column='ValidityTo', blank=True, null=True)
 
     offline = models.BooleanField(db_column='isOffline', blank=True, null=True)
     audit_user_id = models.IntegerField(db_column='AuditUserID')
@@ -42,3 +43,20 @@ class Policy(models.Model):
 
     STAGE_NEW = 'N'
     STAGE_RENEWED = 'R'
+
+    @classmethod
+    def get_queryset(cls, queryset, user):
+        queryset = Policy.filter_queryset(queryset)
+        # GraphQL calls with an info object while Rest calls with the user itself
+        if isinstance(user, ResolveInfo):
+            user = user.context.user
+        if settings.ROW_SECURITY and user.is_anonymous:
+            return queryset.filter(id=-1)
+        # TODO: check the access to the policy information but how ?
+        #   Policy -> Product -> Location ? Policy -> Insurees -> HF -> Location ?
+        # if settings.ROW_SECURITY:
+        #     dist = UserDistrict.get_user_districts(user._u)
+        #     return queryset.filter(
+        #         health_facility__location_id__in=[l.location.id for l in dist]
+        #     )
+        return queryset
