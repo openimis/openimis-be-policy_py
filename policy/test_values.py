fdbcf75fffb892ad65cd47f7df380ed4f2de9ed8
Patrick Delcroix <patrick.delcroix@swisstph.ch>
2023-09-28 11:41:30 +0200

    use ORM for value calculation + fix test (#74)

--- policy/test_values.py
+++ policy/test_values.py
@@ -6,13 +6,15 @@ from product.test_helpers import create_test_product
 from insuree.models import Relation
 from .values import *
 from .test_helpers import create_test_policy
-
+from core.apps import CoreConfig
+from dateutil.relativedelta import relativedelta
 
 class PolicyValuesTestCase(TestCase):
+    test_child_dob=py_datetime.date.today()- relativedelta(years= CoreConfig.age_of_majority-2)
     def test_new_policy_basis(self):
         head_insuree = create_test_insuree(with_family=True, custom_props={"dob": core.datetime.date(1985, 5, 5)})
         spouse = Relation.objects.get(id=8)
-        create_test_insuree(
+        create_test_insuree( with_family = False,
             custom_props={
                 "dob": core.datetime.date(1989, 9, 9),
                 "relationship": spouse,
@@ -39,9 +41,9 @@ class PolicyValuesTestCase(TestCase):
 
         # let's add a child and shift date to 1st of a month
         child = Relation.objects.get(id=4)
-        create_test_insuree(
+        create_test_insuree(with_family = False,
             custom_props={
-                "dob": core.datetime.date(2020, 2, 20),
+                "dob": self.test_child_dob,
                 "relationship": child,
                 "family": head_insuree.family
             })
@@ -56,7 +58,7 @@ class PolicyValuesTestCase(TestCase):
     def test_new_policy_lump_sum_and_cycles(self):
         head_insuree = create_test_insuree(with_family=True, custom_props={"dob": core.datetime.date(1985, 5, 5)})
         spouse = Relation.objects.get(id=8)
-        create_test_insuree(
+        create_test_insuree( with_family = False,
             custom_props={
                 "dob": core.datetime.date(1989, 9, 9),
                 "relationship": spouse,
@@ -89,9 +91,9 @@ class PolicyValuesTestCase(TestCase):
 
         # let's add a child (outside threshold)  and shift date in 1st cycle grace period
         child = Relation.objects.get(id=4)
-        create_test_insuree(
+        create_test_insuree( with_family = False,
             custom_props={
-                "dob": core.datetime.date(2020, 2, 20),
+                "dob": self.test_child_dob,
                 "relationship": child,
                 "family": head_insuree.family
             })
@@ -106,7 +108,7 @@ class PolicyValuesTestCase(TestCase):
     def test_new_policy_admin_period_max_members_insurance_period(self):
         head_insuree = create_test_insuree(with_family=True, custom_props={"dob": core.datetime.date(1985, 5, 5)})
         spouse = Relation.objects.get(id=8)
-        create_test_insuree(
+        create_test_insuree(with_family = False,
             custom_props={
                 "dob": core.datetime.date(1989, 9, 9),
                 "relationship": spouse,
@@ -142,7 +144,7 @@ class PolicyValuesTestCase(TestCase):
         child = Relation.objects.get(id=4)
         create_test_insuree(
             custom_props={
-                "dob": core.datetime.date(2020, 2, 20),
+                "dob": self.test_child_dob,
                 "relationship": child,
                 "family": head_insuree.family
             })
