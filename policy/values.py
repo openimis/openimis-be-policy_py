c210a3b6b689ad2ffeb333be45339685c5d68d49
kevel choumene <k.choumene@cloudfacile.com>
2024-08-29 18:30:55 +0100

    fix: Ajustement erreur variable non definie

--- policy/values.py
+++ policy/values.py
@@ -94,6 +94,7 @@ def family_counts(product, family):
     
     over_children = 0
     over_adults = 0
+    over_other_children = 0
 
     if product.max_members:
         over_adults = max(0,adults   - product.max_members)
