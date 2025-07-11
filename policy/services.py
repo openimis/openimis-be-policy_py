e0ea26aed09d44f3419e0f17de9c60fd3c1ec90b
kevel choumene <k.choumene@cloudfacile.com>
2025-07-11 15:54:55 +0100

    feat: debug

--- policy/services.py
+++ policy/services.py
@@ -1324,6 +1324,7 @@ HOF{% endif %}
 
 
 def update_insuree_policies(policy, audit_user_id):
+    print("Membres ", policy.family.members.filter(validity_to__isnull=True))
     for member in policy.family.members.filter(validity_to__isnull=True):
         existing_ip = InsureePolicy.objects.filter(validity_to__isnull=True, insuree=member, policy=policy).first()
         if existing_ip:
